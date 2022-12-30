02de0260d1f3b2d7596699915642d36879d0c191
Sillery TALLA Philinte <tsillery@ymail.com>
2022-12-30 08:17:40 -0500

    Ajout des Contrats de Services

---
+++ GEPTS.Features/Contrats/Services/IServiceDeMatiere.cs
@@ -0,0 +1,19 @@
+﻿using GEPTS.Features.DTOs.Matieres;
+using MsCommun.Reponses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GEPTS.Features.Contrats.Services
+{
+    public interface IServiceDeMatiere
+    {
+        public Task<List<MatiereDto>> LireToutesLesMatieres();
+        public Task<ReponseDeRequette> AjouterUneMatiere(MatiereACreerDto filiereAAjouter);
+        public Task<ReponseDeRequette> SupprimerUneMatiere(Guid MatiereId);
+        public Task<MatiereDetailDto> LireDetailDuneMatiere(Guid id);
+        public Task<ReponseDeRequette> ModifierUneMatiere(Guid filiereId, MatiereAModifierDto filiereAModifierDto);
+    }
+}
---
+++ GEPTS.Features/Contrats/Services/IServiceDeProgrammation.cs
@@ -0,0 +1,19 @@
+﻿using GEPTS.Features.DTOs.Programmations;
+using MsCommun.Reponses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GEPTS.Features.Contrats.Services
+{
+    public interface IServiceDeProgrammation
+    {
+        public Task<List<ProgrammationDto>> LireToutesLesProgrammations();
+        public Task<ReponseDeRequette> AjouterUneProgrammation(ProgrammationACreerDto filiereAAjouter);
+        public Task<ReponseDeRequette> SupprimerUneProgrammation(Guid ProgrammationId);
+        public Task<ProgrammationDetailDto> LireDetailDuneProgrammation(Guid id);
+        public Task<ReponseDeRequette> ModifierUneProgrammation(Guid filiereId, ProgrammationAModifierDto filiereAModifierDto);
+    }
+}
